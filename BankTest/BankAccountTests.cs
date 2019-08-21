942e5c9a8196b15d7085bde3101a785e8a4063c2
asrulhadi <asrulhadi@teknomatrik.com.my>
2019-08-21 13:07:30 +0800

    Integrated Test

--- BankTest/BankAccountTests.cs
+++ BankTest/BankAccountTests.cs
@@ -5,15 +5,23 @@ namespace Tests
 {
     public class BankAccountTest
     {
+        BankAccount account;
+        double initialAmount = 20.0;
+
         [SetUp]
         public void Setup()
         {
+            account = new BankAccount("Mr Os", initialAmount);
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            account.Debit(10.0);
+
+            double balance = account.Balance;
+
+            Assert.That(balance, Is.EqualTo(10.0).Within(0.001));
         }
     }
 }
